04af469e3549863dc6af873612b3cdf6b87054c6
nametake <nametake.kyarabuki@gmail.com>
2014-05-29 13:42:37 +0900

    complete

--- slideshow.js
+++ slideshow.js
@@ -8,13 +8,17 @@ var slideshow = slideshow || {};
 
     slideshow.nowPage = 0;
     slideshow.pageDiv = [];
+    slideshow.easingSpeed = 300;
+    slideshow.easingType = "swing";
 
     slideshow.init = function () {
         $("#slideshow > div ").each(function (i, ele) {
             slideshow.pageDiv.push($(ele));
-            $(ele).hide();
+            $(ele).css({"left": "100%"});
+
         });
-        slideshow.pageDiv[slideshow.nowPage].show();
+        slideshow.pageDiv[slideshow.nowPage].css({"left": "0"});
+
 
         $("#next").on('click',function(){
             slideshow.next();
@@ -28,15 +32,59 @@ var slideshow = slideshow || {};
         slideshow.changeSlide(slideshow.nowPage + 1);
     }
     slideshow.prev = function () {
-        slideshow.changeSlide(slideshow.nowPage - 1);
+        slideshow.changeSlide(slideshow.nowPage - 2);
     }
 
+    //
     slideshow.changeSlide = function (target) {
-        if (target >= 0 && target < slideshow.pageDiv.length) {
-            slideshow.pageDiv[slideshow.nowPage].hide();
-            slideshow.pageDiv[target].show();
-            slideshow.nowPage = target;
+        // 範囲外であればリターン
+        if (target < 0 || slideshow.pageDiv.length <= target) {
+            return;
         }
-    }
+
+        // 次のスライドに移動する時(左にスライド)
+        if (target > slideshow.nowPage) {
+            // 現在のスライドからターゲットまでを全部左に移動
+            for (var i=slideshow.nowPage+1; i < target; ++i) {
+                slideshow.pageDiv[i].css({"left": "-100%"})
+            }
+            // 次のスライドの移動
+            slideshow.pageDiv[target].animate(
+                {"left": "0"},
+                {
+                    duration: slideshow.easingSpeed,
+                    easing: slideshow.easingType,
+                });
+            // 今のスライドの移動
+            slideshow.pageDiv[slideshow.nowPage].animate(
+                {"left": "-100%"},
+                {
+                    duration: slideshow.easingSpeed,
+                    easing: slideshow.easingType,
+                });
+
+        // 前のスライドに移動する時(右にスライド)
+        } else if(target < slideshow.nowPage){
+            // 現在のスライドからターゲットまでを全部右に移動
+            for (var i=target+1; i < slideshow.nowPage; ++i) {
+                slideshow.pageDiv[i].css({"left": "100%"})
+            }
+            // 次のスライドの移動
+            slideshow.pageDiv[target].animate(
+                {"left": "0"},
+                {
+                    duration: slideshow.easingSpeed,
+                    easing: slideshow.easingType,
+                });
+            // 今のスライドの移動
+            slideshow.pageDiv[slideshow.nowPage].animate(
+                {"left": "100%"},
+                {
+                    duration: slideshow.easingSpeed,
+                    easing: slideshow.easingType,
+                });
+        }
+        slideshow.nowPage = target;
+}
 
 })();
